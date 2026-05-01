7e348352c6ad94a14935e620e2e00eabef6f0bc8
igorbkuhl <igorb.kuhl@gmail.com>
2026-05-01 16:31:36 -0300

    últimas seções e ajuste para mobile

--- clone-disneyplus/src/main.js
+++ clone-disneyplus/src/main.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+    selectTab();
+    toggleAnswer();
+    hideHeroElements();
+});
+
+function selectTab() {
     const buttons = document.querySelectorAll("[data-tab-button]");
 
     for (let button of buttons) {
@@ -11,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
             btn.target.classList.add("shows__tabs__button--is-active");
         })
     }
-});
+}
 
 function removeActiveButton() {
     const buttons = document.querySelectorAll("[data-tab-button]");
@@ -27,4 +33,34 @@ function hideAllTabs() {
     for (let tab of tabsContainer) {
         tab.classList.remove("shows__list--is-active");
     }
+}
+
+function toggleAnswer() {
+    const questions = document.querySelectorAll("[data-faq-question]");
+
+    for (let question of questions) {
+        question.addEventListener("click", (element) => {
+            const openClass = "faq__questions__item--is-open";
+            const parent = element.target.parentNode;
+
+            parent.classList.toggle(openClass);
+        });
+    }
+}
+
+function hideHeroElements() {
+    const heroSection = document.querySelector(".hero");
+    const heroHeight = heroSection.clientHeight;
+
+    window.addEventListener("scroll", () => {
+        const currentPosition = window.scrollY;
+        const header = document.querySelector("header");
+        const hiddenClass = "header--is-hidden";
+
+        if (currentPosition < heroHeight) {
+            header.classList.add(hiddenClass);
+        } else {
+            header.classList.remove(hiddenClass);
+        }
+    });
 }
